9f06d123075e4835d841fa4c7e128994ea635798
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-03-10 16:47:46 +0900

    templateタグ内記述

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    aaa
+    <div>
+      タイトル：
+      <input type="text" />
+    </div>
+    <div>
+      感想：
+      <textarea cols="30" rows="10"></textarea>
+    </div>
+    <div>
+      鑑賞日：
+      <input type="date" />
+    </div>
+    <button type="button">登録する</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class XXXComponent extends Vue {
-
-}
+export default class XXXComponent extends Vue {}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
