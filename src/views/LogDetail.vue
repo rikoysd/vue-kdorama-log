49256dfe07dd71a860cbc29d1806c9f3278c3f29
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-03-10 23:38:13 +0900

    ボタンの設置

--- src/views/LogDetail.vue
+++ src/views/LogDetail.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    
+    <button type="button">編集する</button>
+    <button type="button" v-on:click="backLogList">一覧に戻る</button>
   </div>
 </template>
 
@@ -8,10 +9,13 @@
 import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class XXXComponent extends Vue {
-
+  /**
+   * 一覧ページに戻る.
+   */
+  backLogList(): void {
+    this.$router.push("/logList");
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
