4ecc4280e0752741d12e659c88f2564eec0fbadf
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-03-13 11:43:41 +0900

    app.vueの編集

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
   </div>
 </template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+import Header from "./components/Header.vue";
+import M from "materialize-css/dist/js/materialize.min";
+
+@Component({
+  components: {
+    Header,
+  },
+})
+export default class App extends Vue {
+   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  mounted() {
+    //cdnのインストールが必要。mountedだとタイミングが合わないので、時間をずらした。
+    setTimeout(() => {
+      M.AutoInit();
+    }, 100);
+  }
+}
+</script>
 
 <style></style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    
+    <header>
+      <div class="navi">
+        <router-link>登録</router-link>
+        <div>一覧</div>
+        <div>ログイン</div>
+      </div>
+    </header>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class XXXComponent extends Vue {
-
-}
+export default class XXXComponent extends Vue {}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
