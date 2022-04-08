0e409c8b8a7f5f4a7619138a909161680189bf04
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-04-08 11:58:21 +0900

    cssコピペ

--- src/views/WatchList.vue
+++ src/views/WatchList.vue
@@ -67,4 +67,38 @@ export default class XXXComponent extends Vue {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.items {
+  display: flex;
+  /* 自動的に複数行になる */
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.list {
+  text-align: center;
+  margin-left: 20px;
+  margin-bottom: 20px;
+}
+
+img {
+  width: 200px;
+  height: 300px;
+  /* 各画像の高さを統一 */
+  object-fit: cover;
+  border-radius: 10px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+
+  /* ホバー時にふわっと浮き出る */
+  position: relative;
+  top: 0;
+  display: inline-block;
+  transition: all 0.3s;
+}
+
+img:hover {
+  opacity: 0.8;
+  top: -3px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
+}
+</style>
